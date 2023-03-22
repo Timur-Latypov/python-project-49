896160b14dc92e9d4072e4faf89d96f6836aede1
Timur Latypov <350250431@mail.ru>
2023-03-22 22:42:32 +0500

    fix lint

--- brain_games/game.py
+++ brain_games/game.py
@@ -9,17 +9,19 @@ import brain_games.games.prime
 def welcome(game):
     match game:
         case "even":
-            welcome = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
+            task = "Answer \"yes\" if the number is even, "\
+                "otherwise answer \"no\"."
         case "calc":
-            welcome = "What is the result of the expression? "
+            task = "What is the result of the expression?"
         case "gcd":
-            welcome = "Find the greatest common divisor of given numbers."
+            task = "Find the greatest common divisor of given numbers."
         case "progression":
-            welcome = "What number is missing in the progression?"
+            task = "What number is missing in the progression?"
         case "prime":
-            welcome = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
+            task = "Answer \"yes\" if given number is prime. "\
+                "Otherwise answer \"no\"."
     name = prompt.string("Welcome to the Brain Games!\nMay I have your name? ")
-    print(f"Hello, {name}!\n{welcome}")
+    print(f"Hello, {name}!\n{task}")
     if game_engine(game):
         print(f"Congratulations, {name}!")
     else:
@@ -37,7 +39,8 @@ def game_engine(game):
             case "gcd":
                 (question, true_answer) = brain_games.games.gcd.gcd()
             case "progression":
-                (question, true_answer) = brain_games.games.progression.progression()
+                (question, true_answer) = \
+                    brain_games.games.progression.progression()
             case "prime":
                 (question, true_answer) = brain_games.games.prime.prime()
         print(f"Question: {question}")
@@ -46,7 +49,8 @@ def game_engine(game):
             print("Correct!")
             i += 1
         else:
-            print(f"\'{user_answer}\' is wrong answer ;(. Correct answer was \'{true_answer}\'")
+            print(f"\'{user_answer}\' is wrong answer ;(. "
+                  "Correct answer was \'{true_answer}\'")
             return False
             i = 0
         if i == 3:
