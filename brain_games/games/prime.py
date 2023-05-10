713c920fb5898774a0a7132240e758b78168ec58
Timur Latypov <350250431@mail.ru>
2023-05-10 11:57:17 +0500

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,14 +8,17 @@ GAME_TASK = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
 
 def make_question_and_true_answer():
     question = random.randrange(FIRST_PRIME, RANGE)
-    if is_prime:
+    enumeration_range = range(FIRST_PRIME, question)
+    if is_prime(question, enumeration_range):
         return (question, "yes")
     else:
         return (question, "no")
 
 
-def is_prime(number):
-    for i in range(FIRST_PRIME, number):
+def is_prime(number, enumeration_range):
+    if number == 2:
+        return True
+    for i in enumeration_range:
         if number % i == 0:
             return False
     return True
